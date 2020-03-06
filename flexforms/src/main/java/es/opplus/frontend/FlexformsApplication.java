ff80249e8c4cdd97c16a73a15d451b4b8e079ed4
Antonio <acarvajalperez@gmail.com>
2020-03-06 19:29:33 +0100

    o

---
+++ flexforms/src/main/java/es/opplus/frontend/FlexformsApplication.java
@@ -0,0 +1,13 @@
+package es.opplus.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexformsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexformsApplication.class, args);
+	}
+
+}

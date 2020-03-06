a50f1c1f94a5fe6e680f8994e3758a4ec3f364f1
Antonio <acarvajalperez@gmail.com>
2020-03-06 19:51:03 +0100

    3

--- flexforms/src/main/java/es/opplus/frontend/LogoView.java
+++ flexforms/src/main/java/es/opplus/frontend/LogoView.java
@@ -2,7 +2,9 @@ package es.opplus.frontend;
 
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
 
+@Route("logo")
 public class LogoView extends VerticalLayout {
 	public LogoView() {
 		add(new H1("Prueba"));
--- flexforms/src/main/java/es/opplus/frontend/MainView.java
+++ flexforms/src/main/java/es/opplus/frontend/MainView.java
@@ -1,6 +1,8 @@
 package es.opplus.frontend;
 
 import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.router.Route;
 
+@Route("")
 public class MainView extends AppLayout {
 }
